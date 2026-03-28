e3a7b36de12ca0f145290305da3ab22ff1a035ad
yosh-0827 <yuyuyu.1648@gmail.com>
2026-03-28 19:34:28 +0900

    コメントの追加

--- Counter/app/src/main/java/com/example/counter/MainViewModel.kt
+++ Counter/app/src/main/java/com/example/counter/MainViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
     // 画面の単一の状態源。学習用として Compose で分かりやすい mutableStateOf を使う
     var count by mutableIntStateOf(0)
+        // byはmutableIntStateOf(0) が持っている値の取り出し・更新を、count という変数として使えるようにする
+        // count は外から読めるけど、外から変更はできない、という意味
         private set
 
     fun increment() {
