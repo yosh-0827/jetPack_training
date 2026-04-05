61f983d0beb16695f1b2858f9d9d1756af7d8b99
yosh-0827 <yuyuyu.1648@gmail.com>
2026-04-05 18:39:38 +0900

    databas,dao,entityの作成

---
+++ SimpleToDoApp/app/src/main/java/com/example/simpletodoapp/data/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.simpletodoapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+/**
+ * DBアクセスの入り口。
+ * これを窓口としてアプリ側からこのクラスをインスタンス化しDaoやEntityにアクセスできる。
+ * ルーティングに近い。
+ * */
+@Database(
+    entities = [TodoEntity::class],  // このDBでは TodoEntity を扱います
+    version = 1  // DBのバージョン
+)
+/**
+ * なぜ abstract なのか
+ * ここは最初ちょっと不思議ですが、Room が裏側で実装を作ってくれるからです。
+ *
+ * つまり自分では「こういうDBですよ」「このDaoがありますよ」
+ * と宣言するだけで、実際の処理は Room が生成します。
+ * Dao を interface にしたのと感覚が少し似ています。
+ * */
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+}
---
+++ SimpleToDoApp/app/src/main/java/com/example/simpletodoapp/data/TodoDao.kt
@@ -0,0 +1,32 @@
+package com.example.simpletodoapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * DBへの操作を書く場所
+ * Data Access Object の略
+ * すごく簡単に言うとSQL分を書くところ
+ *
+ * 「Roomの書き込み系は suspend にしておくことが多い」
+ *  DB操作は少し重い処理なので、メインスレッドでそのままやらないため。
+ * */
+@Dao // Daoであることを示す
+interface TodoDao {
+
+    @Query("SELECT * FROM todos")
+    fun getAll(): Flow<List<TodoEntity>>
+
+    @Insert
+    suspend fun insert(todo: TodoEntity)
+
+    @Update
+    suspend fun update(todo: TodoEntity)
+
+    @Delete
+    suspend fun delete(todo: TodoEntity)
+}
